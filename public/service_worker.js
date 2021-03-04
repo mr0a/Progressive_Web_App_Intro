f813be515ac2c8b33357b60c2b595e23f4603839
a <a@me.co>
2021-03-05 01:31:42 +0300

    Dynamic cache with clearOldCache and skipwaiting

--- public/service_worker.js
+++ public/service_worker.js
@@ -1,15 +1,29 @@
-const cacheName = "static-v1";
+const version = 6;
+
+const staticCacheName = `static-v${version}`;
+const dynamicCacheName = `dynamic-v${version}`;
+
 const preCache = [
-    './',
-    './index.html',
-    './assets/vendor/css/bootstrap.min.css',
-    './assets/vendor/js/alpine.js',
-    './assets/js/app.js',
-    './data.json'
+    {
+        name: staticCacheName,
+        urls: [
+            './',
+            './index.html',
+            './assets/vendor/css/bootstrap.min.css',
+            './assets/vendor/js/alpine.js',
+            './assets/js/app.js'
+        ]
+    },
+    {
+        name: dynamicCacheName,
+        urls: [
+            './data.json'
+        ]
+    }
 ];
 
 async function addCacheHeader(res) {
-    if(!res) return;
+    if (!res) return;
 
     let headers = new Headers(res.headers);
     headers.set('sw-cache', 'true');
@@ -23,11 +37,37 @@ async function addCacheHeader(res) {
     });
 }
 
+async function clearOldCache(keysToKeep) {
+    let cacheKeys = await caches.keys();
+    console.log(cacheKeys);
+
+    return Promise.all(
+        cacheKeys
+            .filter(key => keysToKeep.indexOf(key) === -1)
+            .map(key => caches.delete(key))
+    );
+
+}
+
+
 self.addEventListener('install', e => {
     console.log("The SW is installed");
-    e.waitUntil(
-        caches.open(cacheName).then(cache => cache.addAll(preCache))
-    )
+    e.waitUntil(Promise.all(
+        preCache.map(obj => {
+            caches
+                .open(obj.name).then(cache => {
+                    cache.addAll(obj.urls)
+                }).then(() => self.skipWaiting()); // Skipwaiting when new sw is installed
+        })
+    ));
+    // e.waitUntil(
+    //     caches.open(cacheName).then(cache => cache.addAll(preCache))
+    // )
+
+    self.addEventListener('activate', e => {
+        e.waitUntil(clearOldCache(preCache.map(obj => obj.name)))
+    })
+
 });
 
 self.addEventListener('fetch', e => {
